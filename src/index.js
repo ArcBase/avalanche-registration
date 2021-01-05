5af12bec78eb3f0479b08a7b04e30969cb62c958
taiwoadebayo4040@gmail.com <taiwoadebayo4040@gmail.com>
2021-01-05 13:24:44 +0100

    AA

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
 import 'antd/dist/antd.min.css'
+
+import './index.css';
+
+import './assets/Avalanche.css'
+import './assets/pageSections.css'
+import './assets/generals.css'
+import './assets/team.css'
+
+import './assets/Backdrop.css'
+// import './assets/DrawerToggleButton.css'
+import './assets/Navbar.css'
+import './assets/responsive.css'
+import './assets/SideDrawer.css'
+import './assets/Toolbar.css'
+
+// import './assets/components/footer.css'
+// import './assets/components/navbar.css'
+
+import './assets/Slide.css'
+// import './assets/cards.css'
+
+// import './assets/new-navbar.css';
+import './assets/Button.css'
+
+import './assets/ballHive.css'
+import './assets/playerSignUp.css'
+import './assets/landing-page.css'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
